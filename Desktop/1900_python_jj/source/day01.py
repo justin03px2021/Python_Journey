7df34d1bdca71af8868834cf85c55cfd44274d75
Justin Jung <chadwick@Justins-MacBook-Pro.local>
2022-08-04 02:07:12 +0900

    First Commit

---
+++ Desktop/1900_python_jj/source/day01.py
@@ -0,0 +1,32 @@
+"""
+    Day1 : My First Program
+    Version : 1.0
+    Created : 2022.05.12
+    Updated : 20220.05.12
+    Author : Justin Jung
+"""
+
+print('jj')
+
+print('#' * 40)
+print('My First Program')
+print('#' * 40)
+
+my_name = 'Justin Jung'
+your_name = "noname"
+my_name2 = 'Justin "Dangerous" Jung'
+my_name3 = "Justin 'Sleepy' Jung"
+
+print(my_name, your_name)
+print(my_name2)
+print(my_name3)
+
+statement1 = "this is \n very big"
+statement2 = "this is \t very big"
+print(statement1)
+print(statement2)
+
+
+
+
+
